350bf34ae3e3ed0a8b6b474154673d4fdbd820df
david.aspital <david.aspital@ptvgroup.com>
2024-03-27 14:34:34 +0000

    small bug fixes for O00 and Total_Demand attribute

--- CIF2GTFS/import_GTFS.py
+++ CIF2GTFS/import_GTFS.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append(os.path.dirname(__file__))
 
 import get_inputs as gi
-import create_O00 as O00
+import CIF2GTFS.results.create_O00 as O00
 
 
 def createGTFSPuti(gtfsPath, putiFolder):
@@ -148,6 +148,7 @@ def main():
         Visum.Net.VehicleJourneys.AddUserDefinedAttribute(col, col, col, 5)
     Visum.Net.VehicleJourneys.SetMultipleAttributes(journeyDetails.columns.values, journeyDetails.values)
 
+    O00.Visum = Visum
     O00.main()
 
     Visum.IO.SaveVersion(os.path.join(path, 'output\\VISUM\\Network+Timetable.ver'))
